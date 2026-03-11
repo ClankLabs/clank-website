e73ccc41b01211ebbcf8139993cbdabf8d3bba54
ItsTrag1c <tractorscorch@protonmail.com>
2026-03-11 14:42:08 -0400

    Update Build versions: CLI 2.5.14, Desktop 2.4.11

--- lib/versions.ts
+++ lib/versions.ts
@@ -1,8 +1,8 @@
 export const VERSIONS = {
   chat: "0.16.3",
   cli: "0.9.16",
-  build: "2.5.13",
-  buildDesktop: "2.4.10",
+  build: "2.5.14",
+  buildDesktop: "2.4.11",
 } as const;
 
 export type AppKey = keyof typeof VERSIONS;
